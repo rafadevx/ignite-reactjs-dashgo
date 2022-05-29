59e1e274a92f4dad9088c95f9c209d04bcc77dfd
Rafael Del Grossi <rafa.comp@gmail.com>
2022-05-29 11:27:02 -0300

    add responsive padding to dashboar

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -67,11 +67,11 @@ export default function Dashboard() {
         <Sidebar />
 
         <SimpleGrid flex="1" gap="4" minChildWidth="320px" alignItems="flex-start">
-          <Box p="8" bg="gray.800" borderRadius={8} pb="4">
+          <Box p={["6","8"]} bg="gray.800" borderRadius={8} pb="4">
             <Text fontSize="lg" mb="4">Inscritos da Semana</Text>
             <Chart type="area" height={160} options={options} series={series} />
           </Box>
-          <Box p="8" bg="gray.800" borderRadius={8} pb="4">
+          <Box p={["6","8"]} bg="gray.800" borderRadius={8} pb="4">
             <Text fontSize="lg" mb="4">Taxa de abertura</Text>
             <Chart type="area" height={160} options={options} series={series} />
           </Box>
